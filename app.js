0d5140d9b725e82579103d92e5c174e896632668
lasya-lahari <158755249+lasya-lahari@users.noreply.github.com>
2024-02-03 21:06:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,65 @@
-import useSWR from 'swr';
+// App.js
+import React from "react";
+import "./App.css";
+class App extends React.Component {
+	// Constructor
+	constructor(props) {
+		super(props);
 
-// Import useSWR from swr package
+		this.state = {
+			items: [],
+			DataisLoaded: false,
+		};
+	}
 
-// created function to handle API request
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
+	// ComponentDidMount is used to
+	// execute the code
+	componentDidMount() {
+		fetch("https://api.forexrateapi.com/v1/latest?api_key=d6e5a5c33d218c419f377952c5bc7b94&base=USD&currencies=EUR,INR,JPY")
+			.then((res) => res.json())
+			.then((json) => {
+				this.setState({
+					items: json,
+					DataisLoaded: true,
+				});
+			});
+	}
+	render() {
+		const { DataisLoaded, items } = this.state;
+		if (!DataisLoaded)
+			return (
+				<div>
+					<h1> Pleses wait some time.... </h1>
+				</div>
+			);
 
-const Swr = () => {
-  const {
-    data: countries,
-    error,
-    isValidating,
-  } = useSWR('https://api.forexrateapi.com/v1/latest?api_key=d6e5a5c33d218c419f377952c5bc7b94&base=USD&currencies=EUR,INR,JPY', fetcher);
+		return (
+			<div className="App">
+				<h1 className="geeks">GeeksforGeeks</h1>
+				<h3>Fetch data from an api in react</h3>
+				<div className="container">
+					{items.map((item) => (
+						<div className="item">
+							<ol key={item.id}>
+								<div>
+									<strong>
+										{"User_Name: "}
+									</strong>
+									{item.username},
+								</div>
+								<div>
+									Full_Name: {item.name},
+								</div>
+								<div>
+									User_Email: {item.email}
+								</div>
+							</ol>
+						</div>
+					))}
+				</div>
+			</div>
+		);
+	}
+}
 
-  // Handles error and loading state
-  if (error) return <div className='failed'>failed to load</div>;
-  if (isValidating) return <div className="Loading">Loading...</div>;
-
-  return (
-    <div>
-      {countries &&
-        countries.map((country, index) => (
-          <img key={index} src={country.flags.png} alt='flag' width={100} />
-        ))}
-    </div>
-  );
-};
-
-export default Swr;
+export default App;
